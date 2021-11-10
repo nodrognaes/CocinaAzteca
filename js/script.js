a29eff9c7accec861585f9decf161214be597f44
Sean Gordon <nodrog.naes@gmail.com>
2021-11-10 18:23:56 -0500

    dropdown and logo fix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
+const dropbtn = document.querySelector('.dropbtn');
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
+    dropbtn.classList.toggle("dDActive");
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
+        for (var i = 0; i < dropdowns.length; i++) {
             var openDropdown = dropdowns[i];
             if (openDropdown.classList.contains('show')) {
                 openDropdown.classList.remove('show');
